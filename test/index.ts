679e0d7edf6e287f48f02f68b699c8db3cf15c62
GervinFung <gervinfungdaxuen@gmail.com>
2023-08-06 08:20:49 +0800

    test(config): updated test to run in parallel

--- test/index.ts
+++
@@ -1,7 +0,0 @@
-import testCases from 'cases-of-test';
-import testNode from './node';
-import testReact from './react';
-
-testCases({
-    tests: [[testNode], [testReact], [testStyledComponentsJsx]],
-});
