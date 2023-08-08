ef423141728a863cb22777e14c688de264f5428a
PoolOfDeath20 <53143214+GervinFung@users.noreply.github.com>
2023-08-08 11:33:52 +0800

    Generate prettier config (#11)
    * feat(pretter-config-generator): replace `prettier-config` with a prettier config generator
    
    * fix(package-lock)): update lock file
    
    * fix(lint-on-supposingly-ignored-files): remove files that should be ignored by git
    
    * fix(build): cd into prettier proper directory

---
+++ packages/prettier-config-generator/src/index.ts
@@ -0,0 +1,8 @@
+import fs from 'fs';
+import config from './prettier-config';
+
+const main = () => {
+	fs.writeFileSync('./.prettierrc', JSON.stringify(config, undefined, 4));
+};
+
+export default main;
