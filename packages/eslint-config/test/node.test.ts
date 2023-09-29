ec5305070a529b6013fb67e2a76d027440737ceb
GervinFung <gervinfungdaxuen@gmail.com>
2023-09-29 20:54:20 +0800

    refactor(eslint-config): removed test

--- packages/eslint-config/test/node.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import base from '../src/base';
-import { node } from '../src/node';
-
-describe('node config', () => {
-	it('should assert that node config is correct', () => {
-		expect(node).toStrictEqual(base);
-	});
-});
