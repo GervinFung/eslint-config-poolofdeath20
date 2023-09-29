e8ac0dff45a92b80a2c969487577af56b98209ab
GervinFung <gervinfungdaxuen@gmail.com>
2023-09-29 20:36:06 +0800

    feat(eslint-config): added config for nextjs

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig(() => {
+	const timeOut = 300_000;
+
+	return {
+		test: {
+			watch: false,
+			testTimeout: timeOut,
+			hookTimeout: timeOut,
+		},
+	};
+});
