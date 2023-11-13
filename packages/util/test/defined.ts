c7708b4b0a47b85363127ebdb5758c0448b282ca
GervinFung <gervinfungdaxuen@gmail.com>
2023-11-13 21:38:00 +0800

    feat(util/defined): allow `Defined` to map values

--- packages/util/test/defined.ts
+++ packages/util/test/defined.ts
@@ -3,12 +3,28 @@ import { Defined } from '../src/defined';
 
 describe('Define structure', () => {
 	it('should handle empty value with an `else` path', () => {
-		expect(Defined.parse(1).orGet(2)).toBe(1);
+		expect(
+			Defined.parse(1)
+				.map((id) => {
+					return {
+						id,
+					};
+				})
+				.orGet(2)
+		).toStrictEqual({
+			id: 1,
+		});
 
 		expect(
-			Defined.parse(undefined).orElse(() => {
-				return 'A';
-			})
+			Defined.parse(undefined as string | undefined)
+				.map((string) => {
+					return {
+						list: string.split(''),
+					};
+				})
+				.orElse(() => {
+					return 'A';
+				})
 		).toBe('A');
 
 		expect(() => {
