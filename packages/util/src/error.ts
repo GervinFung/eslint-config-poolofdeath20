cce64eaee5b8099c377f7d81edef4410d5cd62b0
GervinFung <gervinfungdaxuen@gmail.com>
2023-11-18 00:47:06 +0800

    fix(format): formatted files with `prettier`

--- packages/util/src/error.ts
+++ packages/util/src/error.ts
@@ -2,8 +2,8 @@ const processErrorMessage = (error: unknown) => {
 	return typeof error === 'string'
 		? error
 		: error instanceof Error
-		? error.message
-		: JSON.stringify(error);
+		  ? error.message
+		  : JSON.stringify(error);
 };
 
 export { processErrorMessage };
