9f66e16cf8169711437881b5794d1303695046af
GervinFung <gervinfungdaxuen@gmail.com>
2023-12-21 12:05:54 +0800

    lint(any): disable `eslint` for rest arg

--- packages/util/src/type.ts
+++ packages/util/src/type.ts
@@ -16,6 +16,7 @@ type DeepReadonlyObject<T> = {
 	readonly [P in keyof T]: DeepReadonly<T[P]>;
 };
 
+/* eslint-disable  @typescript-eslint/no-explicit-any */
 type Argument<T extends (...parameters: any) => unknown> = Parameters<T>[0];
 
 export type { Argument };
