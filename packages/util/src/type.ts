3d4658a87a4d11f0cf478d28b7dc8b7553549fdf
PoolOfDeath20 <53143214+GervinFung@users.noreply.github.com>
2023-12-21 22:40:09 +0800

    Update package (#45)
    * chore(packages): update to latest non-breaking versions
    
    * chore(packages): update to latest breaking versions
    
    * format(*): formatted all files with `prettier`

--- packages/util/src/type.ts
+++ packages/util/src/type.ts
@@ -5,12 +5,12 @@ const nullToUndefined = <T>(value: NonNullable<T> | null) => {
 type DeepReadonly<T> = T extends (infer R)[]
 	? ReadonlyArray<DeepReadonly<R>>
 	: T extends Set<infer R>
-	  ? ReadonlySet<DeepReadonly<R>>
-	  : T extends Function
-	    ? T
-	    : T extends object
-	      ? DeepReadonlyObject<T>
-	      : T;
+		? ReadonlySet<DeepReadonly<R>>
+		: T extends Function
+			? T
+			: T extends object
+				? DeepReadonlyObject<T>
+				: T;
 
 type DeepReadonlyObject<T> = {
 	readonly [P in keyof T]: DeepReadonly<T[P]>;
